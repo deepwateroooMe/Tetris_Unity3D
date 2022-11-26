794c218edc9c6bb55e1e3edc80af4bc4fbbe6961
deepwaterooo <heyan.huang2020@gmail.com>
2022-11-25 22:37:38 -0800

    LOVE MY DEAR COUSIN. android SDK design -- initial. LOVE MY DEAR COUSIN. Must MARRY HIM -- MY DEAR BELOVED COUSIN

--- trunk/client/trunk/Assets/Scripts/deepwaterooo/tetris3d/MenuBtnsCallback.cs
+++ trunk/client/trunk/Assets/Scripts/deepwaterooo/tetris3d/MenuBtnsCallback.cs
@@ -15,6 +15,7 @@ namespace deepwaterooo.tetris3d {
         Button eduButton; // Education
         Button claButton; // Classic
         Button chaButton; // Challenge
+
         [SerializeField]
         public GameApplication ga;
         
@@ -32,40 +33,18 @@ namespace deepwaterooo.tetris3d {
             Debug.Log(TAG + " OnClickEduButton()");
             ga.HotFix.startEducational();
             gameObject.SetActive(false);
-            // GloData.Instance.saveGamePathFolderName = "educational/grid";
-            // GloData.Instance.camPos.Value = new Vector3(14.10899f, 23.11789f, -1.698298f);
-            // GloData.Instance.camRot.Value = Quaternion.Euler(new Vector3(490.708f, -251.184f, -539.973f));
-            // menuViewPanel.SetActive(false);
-            // educaModesViewPanel.SetActive(true);
-            // GloData.Instance.gameMode.Value = 0;
-            // GameView.nextTetrominoSpawnPos = new Vector3(2.0f, Model.gridHeight - 1f, 2.0f);
-            // ViewManager.SettingsView.Hide();
         }
+
         void OnClickClaButton() { // CLASSIC MODE
             Debug.Log(TAG + " OnClickClassicButton()");
             ga.HotFix.startClassical();
                 gameObject.SetActive(false);
-            // GloData.Instance.saveGamePathFolderName = "classic/level";
-            // GloData.Instance.gridSize.Value = 5;
-            // GloData.Instance.gridXSize = 5;
-            // GloData.Instance.gridZSize = 5;
-            // GloData.Instance.camPos.Value = new Vector3(14.10899f, 23.11789f, -1.698298f);
-            // GloData.Instance.camRot.Value = Quaternion.Euler(new Vector3(490.708f, -251.184f, -539.973f));
-            // GloData.Instance.gameMode.Value = 1;
-            // GameView.nextTetrominoSpawnPos = new Vector3(2.0f, Model.gridHeight - 1f, 2.0f);
-            // offerGameLoadChoice();
-            // ViewManager.SettingsView.Hide();
         }
+
         void OnClickChaButton() { // CHALLENGE MODE
             Debug.Log(TAG + " OnClickClallengeButton()");
             ga.HotFix.startChallenging();
             gameObject.SetActive(false);
-            // GloData.Instance.saveGamePathFolderName = "challenge/level";
-            // GloData.Instance.isChallengeMode = true;
-            // ViewManager.ChallLevelsView.Reveal();
-            // GloData.Instance.gameMode.Value = 0;
-            // ViewManager.SettingsView.Hide();
-            // Hide();
         }
 #endregion
     }
